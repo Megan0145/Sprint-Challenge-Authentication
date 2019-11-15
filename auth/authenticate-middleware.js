5b71c61540913ed526b088261ef1f89738680926
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-15 09:04:04 +0000

    Authenticate middleware. Can now get dad jokes if token passed in authorization header

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,24 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, process.env.SECRET, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: "Bad token. " + err.message });
+      } else {
+        req.decodedToken = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res
+      .status(401)
+      .json({ message: "You shall not pass! No credentials provided" });
+  }
 };
