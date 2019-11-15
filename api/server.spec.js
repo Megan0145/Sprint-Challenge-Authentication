8917e276091be83eefecbe5dc9cbbb92e80cc531
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-15 09:37:16 +0000

    Test: Register endpoint returns status code 401 when no credentials provided

--- api/server.spec.js
+++ api/server.spec.js
@@ -2,11 +2,14 @@ const request = require("supertest");
 const server = require("./server");
 
 describe("server", () => {
-    test("the db env is testing", () => {
-        expect(process.env.DB_ENV).toBe("testing")
-    })
+  test("the db env is testing", () => {
+    expect(process.env.DB_ENV).toBe("testing");
+  });
 
-    describe("login endpoint", () => {
-
-    })
-})
+  describe("register endpoint", () => {
+    test("Should return 401 when no credentials provided", async () => {
+      const response = await request(server).post("/api/auth/register");
+      expect(response.status).toBe(401);
+    });
+  });
+});
