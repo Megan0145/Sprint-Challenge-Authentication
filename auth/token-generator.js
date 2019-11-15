dc610f638303284fb4dac0951e24ef28ae989b39
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-15 08:50:40 +0000

    Token generator function

---
+++ auth/token-generator.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+
+  const options = {
+    expiresIn: "1d"
+  };
+
+  const result = jwt.sign(payload, process.env.SECRET, options);
+
+  return result;
+}
+
+module.exports = generateToken;
