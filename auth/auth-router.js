ab5a9a1afa32e5dbe6e5ff64f91699a4a2fa24de
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-15 08:33:10 +0000

    Endpoint to register new user, hashes pw before saving to db. Middleware function to validate request body

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,42 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const users = require("./auth-model");
+const bcrypt = require("bcryptjs");
 
-router.post('/register', (req, res) => {
+router.post("/register", validateUser, (req, res) => {
   // implement registration
+  const hashpw = bcrypt.hashSync(req.body.password, 10);
+
+  const newUser = {
+    username: req.body.username,
+    password: hashpw
+  };
+
+  users
+    .add(newUser)
+    .then(user => {
+      res.status(201).json({ message: "Account created!", user });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: "Could not create new user: " + err.message });
+    });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
 });
+
+function validateUser(req, res, next){
+  if (!Object.keys(req.body).length) {
+    res.status(401).json({ message: "Missing user data" });
+  } else if (!req.body.username) {
+    res.status(401).json({ message: "Missing required username" });
+  } else if (!req.body.password) {
+    res.status(401).json({ message: "Missing required password" });
+  } else {
+    next();
+  }
+}
 
 module.exports = router;
